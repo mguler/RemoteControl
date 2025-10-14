603d589411532cff9c228638a04bf26e798470e8
mguler <mguler@mguler>
2025-10-14 13:20:46 -0400

    CursorInfo struct added

---
+++ RemoteControl.CaptureService/CursorInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RemoteControl.CaptureService
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct CursorInfo
+    {
+        public int cbSize;
+        public int flags;
+        public IntPtr hCursor;
+        public Point ptScreenPos;
+    }
+}
